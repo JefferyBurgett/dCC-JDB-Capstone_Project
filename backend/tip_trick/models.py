c2194057fb6b48842356d28744de73483a4b5d0e
JefferyBurgett <jeffery.burgett@gmail.com>
2023-02-04 22:51:43 -0600

    Added Model + Serializer for Tip_Trick

--- backend/tip_trick/models.py
+++ backend/tip_trick/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from diver.models import Diver
 
 # Create your models here.
+class Tip_Trick(models.Model):
+    tt_author = models.ForeignKey(Diver, on_delete=models.CASCADE)    
+    tt_text = models.CharField
